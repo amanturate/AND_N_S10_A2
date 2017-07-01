055fcc7073354e72f6f48986ad3f72f515eb8a45
Aman Turate <turateaman1995@gmail.com>
2017-07-01 16:14:05 +0530

    AND_N_S10_A2

---
+++ app/src/main/java/com/example/turate/and_n_s10_a2/Layout4.java
@@ -0,0 +1,22 @@
+package com.example.turate.and_n_s10_a2;
+
+/**
+ * Created by turat on 01-07-2017.
+ */
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class Layout4 extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+// TODO Auto-generated method stub
+        return  inflater.inflate(R.layout.layout4, container, false);
+
+    }
+}
